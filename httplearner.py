20b857a827aad1c7d281688a548e8bc13e0d6d3c
Arun-Misra <kanhaam2006@gmail.com>
2025-11-28 03:44:16 +0530

    Learn to Get website info

--- httplearner.py
+++ httplearner.py
@@ -1,3 +1,14 @@
 import socket
+import re
 
-mysock = 
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('httpforever.com', 80))
+get_demo = 'GET http://httpforever.com/ HTTP/1.0\n\n'.encode()
+mysock.send(get_demo)
+
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    print(data.decode())
+mysock.close()
