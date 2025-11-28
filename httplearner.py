6daef3945a34cc4b931718b55b48af9b47eb1f59
Arun-Misra <kanhaam2006@gmail.com>
2025-11-29 04:41:37 +0530

    Build a simple server as well as GET info from it

--- httplearner.py
+++ httplearner.py
@@ -2,13 +2,12 @@ import socket
 import re
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('httpforever.com', 80))
-get_demo = 'GET http://httpforever.com/ HTTP/1.0\n\n'.encode()
+mysock.connect(('localhost', 9000))
+get_demo = 'GET / HTTP/1.1\r\nHost: localhost\r\n\r\n'.encode()
 mysock.send(get_demo)
 
-while True:
-    data = mysock.recv(512)
-    if (len(data) < 1):
-        break
-    print(data.decode())
+# while True:
+data = mysock.recv(5124)
+strr = data.decode()
+print(strr)
 mysock.close()
