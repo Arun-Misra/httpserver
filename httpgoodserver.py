48143fea67b8b5d28a4327b240d5ca6911c1922c
Arun-Misra <kanhaam2006@gmail.com>
2025-11-30 06:04:17 +0530

    Add better segregation for headers and body, Add POST method to accept JSON

--- httpgoodserver.py
+++ httpgoodserver.py
@@ -1,8 +1,12 @@
 import socket
 import re
+import json
 
 HOST = "localhost"
 PORT = 8080
+
+json_data_store = {}
+id = 1
 
 # 1. Socket establishment
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,38 +21,50 @@ connection.listen(5)
 print(f"Listening to port {PORT}")
 
 def response_build(http_status, content, content_type='text/html'):
-    response= (
+    body_in_byte = content.encode() if isinstance(content, str) else json.dumps(content).encode()
+    headers= (
         f"HTTP/1.1 {http_status}\r\n"
         f"Content-Type: {content_type}; charset=utf-8\r\n"
-        f"Content-Length: {len(content.encode())}\r\n"
+        f"Content-Length: {len(body_in_byte)}\r\n"
         "\r\n"
-        f"{content}"
+        # f"{body_in_byte}"
     )
-    return response
+    return headers.encode() + body_in_byte
 
 while True:
     socket_connect, address = connection.accept()
-    req =  socket_connect.recv(1000).decode()
-    headers = req.split("\r\n")
-    if len(headers) > 0:
-        print(headers[0])
-    first_header = headers[0].split()
-    print(first_header)
+    request =  socket_connect.recv(1000).decode()
+    lines = request.split("\r\n")
+    first_header = lines[0].split()
     http_method = first_header[0]
     path = first_header[1]
+    headers = {}
+    i = 1
+    while lines[i]:
+        k, v = lines[i].split(":", 1)
+        headers[k.strip()] = v.strip()
+        i += 1
+    body = "\r\n".join(lines[i+1:])
+
     
-    if http_method == 'GET':
-        if path == '/':
-            with open("ff.html", "r") as fin:
-                content = fin.read()
-            response = response_build("200 OK", content)
-            
-        elif path.startswith('/echo'):
-            check = re.findall("[=](.+)", path)
-            content = check[0]
-            response = response_build("200 OK", content)
+    if  http_method == 'GET'and path == '/' :
+        # with open("ff.html", "r") as fin:
+        #     content = fin.read()
+        response = response_build("200 OK", "<h1>WELCOME TO MY SERVER</h1>")
+
+    elif http_method == 'GET' and path.startswith('/echo'):
+        check = re.findall("[=](.+)", path)
+        content = check[0]
+        response = response_build("200 OK", content)
+        
+    elif http_method == 'POST' and path == '/data':
+        object = json.loads(body)
+        object["id"] = id
+        json_data_store[id] = object
+        id+=1
+        response = response_build("200 OK", {"status": "success", "id": object["id"]}, "application/json")
             
     else:
         response = 'HTTP/1.1 405 Method Not Allowed\r\n\r\nAllow: GET'
-    socket_connect.sendall(response.encode())
+    socket_connect.sendall(response)
     socket_connect.close()
